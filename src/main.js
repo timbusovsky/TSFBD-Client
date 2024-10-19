11f8fcb5b28d90ee013128ee2dfeda046bf40f82
timbusovsky <tim.busovsky@gmail.com>
2024-10-18 22:47:09 -0400

    Adding tracking

--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Particles from "vue3-particles"
 
-const app = createApp(App)
+const app = createApp(App).use(Particles)
 
 app.use(router)
 
